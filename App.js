596a5a3c93742b9ba5c68683d842843166b33713
Coder-Rushabh <rushabhdabhade30@gmail.com>
2024-03-15 20:46:29 +0530

    problem with metro

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-
+import { registerRootComponent } from 'expo';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './App/Screen/Login'
+import Login from './App/Screen/Login';
+
+const Stack = createStackNavigator(); // Define Stack navigator
 
 export default function App() {
   return (
-    <>
-     <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
-    </>
   );
 }
+
+registerRootComponent(App); // Register the root component
