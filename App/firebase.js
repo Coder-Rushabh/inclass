7497fb6fa31aaab4cd3ca47e2531de8d916ff409
Rushabh Dabhade <rushabhdabhade30@gmail.com>
2024-04-13 16:27:45 +0530

    Update firebase.js

--- App/firebase.js
+++ App/firebase.js
@@ -5,12 +5,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_7y1ucltugtGcp1nZeRRkOn59Ubr78K0",
-  authDomain: "inclass-be45b.firebaseapp.com",
-  projectId: "inclass-be45b",
-  storageBucket: "inclass-be45b.appspot.com",
-  messagingSenderId: "534547516203",
-  appId: "1:534547516203:web:cf62def35ba918d906d36e"};
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
